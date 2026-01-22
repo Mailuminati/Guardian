3cf3bc38af4ad8245b9bc5add8fdee07c56f2571
bressiers <63661165+bressiers@users.noreply.github.com>
2026-01-22 20:38:06 +0100

    Bump version to 0.5.7 in installer and globals
    Updated GUARDIAN_VERSION in install.sh and EngineVersion in globals.go to 0.5.7 to reflect the new release.

--- mi_guardian/globals.go
+++ mi_guardian/globals.go
@@ -26,7 +26,7 @@ import (
 
 // --- Mailuminati engine configuration ---
 const (
-	EngineVersion         = "0.5.5"
+	EngineVersion         = "0.5.7"
 	FragKeyPrefix         = "mi_f:"
 	LocalFragPrefix       = "lg_f:"
 	OracleCacheFragPrefix = "oc_f:"
